eb28ebd72eb190c92067423bc9ad6949048085b6
UroobaShameem <uroobashameem12@gmail.com>
2023-05-16 22:21:37 +0500

    commit

---
+++ loop.py
@@ -0,0 +1,11 @@
+import re
+
+pattern = r'^for\s*\(\s*(num|decimal|letter)\s+(?![0-9_])[_a-zA-Z$][_a-zA-Z0-9$]*\s*=\s*(-?\d+(\.\d+)?|\'[a-zA-Z]\'|\"[a-zA-Z]+\")\s*,\s*(?![0-9_])[_a-zA-Z$][_a-zA-Z0-9$]*\s+(>|<|<=|>=|==|!=)\s+(\d+\.\d+)\s\)\s*$'
+
+# Example usage
+text = input("Enter a for loop statement: ")
+matches = re.match(pattern, text)
+if matches:
+    print("Match found!")
+else:
+    print("No match.")
