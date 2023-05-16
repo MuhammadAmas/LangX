cf45aaedeb6a595b2e976ca60159d20a063e890f
UroobaShameem <uroobashameem12@gmail.com>
2023-05-16 22:40:16 +0500

    commit

--- loop.py
+++ loop.py
@@ -1,6 +1,6 @@
 import re
 
-pattern = r'^for\s*\(\s*(num|decimal|letter)\s+(?![0-9_])[_a-zA-Z$][_a-zA-Z0-9$]*\s*=\s*(-?\d+(\.\d+)?|\'[a-zA-Z]\'|\"[a-zA-Z]+\")\s*,\s*(?![0-9_])[_a-zA-Z$][_a-zA-Z0-9$]*\s+(>|<|<=|>=|==|!=)\s+(\d+\.\d+)\s\)\s*$'
+pattern = r'^for\s*\(\s*(num|decimal|letter)\s+(?![0-9_])[_a-zA-Z$][_a-zA-Z0-9$]*\s*=\s*(-?\d+(\.\d+)?|\'[a-zA-Z]\'|\"[a-zA-Z]+\")\s*,\s*(?![0-9_])[_a-zA-Z$][_a-zA-Z0-9$]*\s*(>|<|<=|>=|==|!=)\s*(\d+)\s*\)\s*,\s*(?![0-9_])[_a-zA-Z$][_a-zA-Z0-9$]\s*(\+\+|--)\s*\)\s*$'
 
 # Example usage
 text = input("Enter a for loop statement: ")
